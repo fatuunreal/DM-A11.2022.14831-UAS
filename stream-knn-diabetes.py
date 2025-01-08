c20c41ba949094625c373fc049f376f7cd8246f1
fatuunreal <faturach03@gmail.com>
2025-01-09 05:02:03 +0700

    Update stream-knn-diabetes.py

--- stream-knn-diabetes.py
+++ stream-knn-diabetes.py
@@ -10,39 +10,54 @@ st.title('Prediksi Diabetes Dengan KNN')
 #membagi kolom
 col1, col2 = st.columns(2)
 
-with col1 :
-    Pregnancies = st.text_input ('input nilai Pregnancies')
+with col1:
+    Pregnancies = st.text_input('Input nilai Pregnancies', '0')
 
-with col2 :
-    Glucose = st.text_input ('input nilai Glucose')
+with col2:
+    Glucose = st.text_input('Input nilai Glucose', '0')
 
-with col1 :
-    BloodPressure = st.text_input ('input nilai Blood Pressure')
+with col1:
+    BloodPressure = st.text_input('Input nilai Blood Pressure', '0')
 
-with col2 :
-    SkinThickness = st.text_input ('input nilai Skin Thickness')
+with col2:
+    SkinThickness = st.text_input('Input nilai Skin Thickness', '0')
 
-with col1 :
-    Insulin = st.text_input ('input nilai Insulin')
+with col1:
+    Insulin = st.text_input('Input nilai Insulin', '0')
 
-with col2 :
-    BMI = st.text_input ('input nilai BMI')
+with col2:
+    BMI = st.text_input('Input nilai BMI', '0')
 
-with col1 :
-    DiabetesPedigreeFunction = st.text_input ('input nilai Diabetes Pedigree Function')
+with col1:
+    DiabetesPedigreeFunction = st.text_input('Input nilai Diabetes Pedigree Function', '0.0')
 
-with col2 :
-    Age = st.text_input ('input nilai Age')
+with col2:
+    Age = st.text_input('Input nilai Age', '0')
 
 # code untuk prediksi
 diab_diagnosis = ''
 
 # membuat tombol untuk prediksi
 if st.button('Test Prediksi Diabetes'):
-    diab_prediction = diabetes_model.predict([[Pregnancies, Glucose, BloodPressure, SkinThickness, Insulin, BMI, DiabetesPedigreeFunction, Age]])
+    try:
+        # Konversi input ke tipe numerik
+        Pregnancies = int(Pregnancies)
+        Glucose = float(Glucose)
+        BloodPressure = float(BloodPressure)
+        SkinThickness = float(SkinThickness)
+        Insulin = float(Insulin)
+        BMI = float(BMI)
+        DiabetesPedigreeFunction = float(DiabetesPedigreeFunction)
+        Age = int(Age)
 
-    if(diab_prediction[0] == 1):
-        diab_diagnosis = 'Pasien terkena Diabetes'
-    else:
-        diab_diagnosis = 'Pasien tidak terkena Diabetes'
-st.success(diab_diagnosis)
+        # Prediksi
+        diab_prediction = diabetes_model.predict([[Pregnancies, Glucose, BloodPressure, SkinThickness, Insulin, BMI, DiabetesPedigreeFunction, Age]])
+
+        if diab_prediction[0] == 1:
+            diab_diagnosis = 'Pasien terkena Diabetes'
+        else:
+            diab_diagnosis = 'Pasien tidak terkena Diabetes'
+        st.success(diab_diagnosis)
+
+    except ValueError:
+        st.error("Harap masukkan nilai numerik yang valid.")
