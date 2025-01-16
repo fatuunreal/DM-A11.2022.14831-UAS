8f16c349c9f16add5aa34ddabb637459ec2a39c4
fatuunreal <faturach03@gmail.com>
2025-01-16 20:45:26 +0700

    Update stream-knn-diabetes.py

--- stream-knn-diabetes.py
+++ stream-knn-diabetes.py
@@ -7,30 +7,30 @@ import matplotlib.pyplot as plt
 diabetes_model = pickle.load(open('knn_diabetes_model.sav', 'rb'))
 
 # Judul aplikasi
-st.title('Prediksi Diabetes Dengan KNN wadidaw')
+st.title('Prediksi Diabetes Dengan KNN')
 
 # Membagi kolom untuk input
 col1, col2 = st.columns(2)
 
 with col1:
-    Pregnancies = st.text_input('Input nilai Pregnancies', '0')
+    Pregnancies = st.text_input('Input nilai Pregnancies')
 with col2:
-    Glucose = st.text_input('Input nilai Glucose', '0')
+    Glucose = st.text_input('Input nilai Glucose')
 
 with col1:
-    BloodPressure = st.text_input('Input nilai Blood Pressure', '0')
+    BloodPressure = st.text_input('Input nilai Blood Pressure')
 with col2:
-    SkinThickness = st.text_input('Input nilai Skin Thickness', '0')
+    SkinThickness = st.text_input('Input nilai Skin Thickness')
 
 with col1:
-    Insulin = st.text_input('Input nilai Insulin', '0')
+    Insulin = st.text_input('Input nilai Insulin')
 with col2:
-    BMI = st.text_input('Input nilai BMI', '0')
+    BMI = st.text_input('Input nilai BMI')
 
 with col1:
-    DiabetesPedigreeFunction = st.text_input('Input nilai Diabetes Pedigree Function', '0')
+    DiabetesPedigreeFunction = st.text_input('Input nilai Diabetes Pedigree Function')
 with col2:
-    Age = st.text_input('Input nilai Age', '0')
+    Age = st.text_input('Input nilai Age')
 
 # Variabel untuk hasil diagnosis
 diab_diagnosis = ''
